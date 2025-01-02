bdb3a4ddcd6dd85d6d605e12f0503d0a93079bb0
AnandaHersa <hersatandiabang@gmail.com>
2025-01-02 18:22:50 +0700

    Update app.blade.php

--- RelieveU/resources/views/layouts/app.blade.php
+++ RelieveU/resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Dashboard</a>
+                    <a class="nav-link" href="/dashboard">Dashboard</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Logout</a>
